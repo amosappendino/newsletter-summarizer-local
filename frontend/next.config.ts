8f9915b451fb07e3edd863b53ee8b1b8b7504aa3
amosappendino <amosappe@mit.edu>
2025-01-26 20:55:59 -0500

    debugging to allow vercel links

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -3,6 +3,22 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
     env: {
         NEXT_PUBLIC_BACKEND_URL: 'https://newsletter-summarizer-1081940379388.us-central1.run.app',
+        NEXT_PUBLIC_FRONTEND_URL: 'https://newsletter-summarizer-omega.vercel.app'
+    },
+    async redirects() {
+        return [
+            {
+                source: '/:path*',
+                has: [
+                    {
+                        type: 'host',
+                        value: '(?!newsletter-summarizer-omega.vercel.app).*',
+                    },
+                ],
+                destination: 'https://newsletter-summarizer-omega.vercel.app/:path*',
+                permanent: true,
+            },
+        ];
     },
     async rewrites() {
         return [
